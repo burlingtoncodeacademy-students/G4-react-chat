4042091f4d21a5fa5ba1a4cbe336165e8a8d8e19
Mi-Stap <mikeystaples@gmail.com>
2024-02-11 18:53:43 -0500

    removed some files and updated app file

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import Auth from "./components/Auth"; // Import Auth component
 
 function App() {
   // Function to update token in localStorage
@@ -9,20 +10,9 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Other components can also be added here */}
+      <Auth updateToken={updateToken} />
+      {/* Further components can be rendered here */}
     </div>
   );
 }
